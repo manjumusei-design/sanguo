3f704c177bf3fb27889a8cfa56cbc0c5e4c38733
Jiga <manjumusei@gmail.com>
2026-04-22 22:33:02 +0800

    Refactor HUDManager by removing unnecessary whitespace and ensuring consistent formatting

--- ui/HUDManager.ts
+++ ui/HUDManager.ts
@@ -8,7 +8,6 @@ export class HUDManager {
   private topBar!: Phaser.GameObjects.Container;
   private topBarBg!: Phaser.GameObjects.Graphics;
 
-
   private qiOrb!: Phaser.GameObjects.Container;
   private qiText!: Phaser.GameObjects.Text;
   private hpBarBg!: Phaser.GameObjects.Graphics;
@@ -17,3 +16,27 @@ export class HUDManager {
   private blockText!: Phaser.GameObjects.Text;
   private hpBarWidth = 220;
   private hpBarHeight = 24;
+
+  // Card pile
+  private drawPileContainer!: Phaser.GameObjects.Container;
+  private drawPileCountText!: Phaser.GameObjects.Text;
+  private discardPileContainer!: Phaser.GameObjects.Container;
+  private discardPileCountText!: Phaser.GameObjects.Text;
+  private onDrawClick?: () => void;
+
+  // Relic rack
+  private relicContainer!: Phaser.GameObjects.Container;
+  private relicItems: Phaser.GameObjects.Container[] = [];
+
+  // Power display
+  private powerContainer!: Phaser.GameObjects.Container;
+  private powerTexts: Phaser.GameObjects.Text[] = [];
+
+  // Status display
+  private statusContainer!: Phaser.GameObjects.Container;
+  private statusText!: Phaser.GameObjects.Text;
+  private lastEnergy = 3;
+
+  constructor(scene: Phaser.Scene) {
+    this.scene = scene;
+  }
